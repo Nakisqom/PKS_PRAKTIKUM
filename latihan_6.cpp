9af11713ef7e59c9f4adecbaac4a94466f0c3658
Thorak <97943216+Nakisqom@users.noreply.github.com>
2023-03-06 10:44:51 +0700

    Add files via upload

---
+++ latihan_6.cpp
@@ -0,0 +1,16 @@
+//LATIHAN 06-02-23
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int bilangan;
+	cout<<"Inputkan bilangan :";
+	cin>>bilangan;
+	
+	if(bilangan%2==0){
+		cout<<bilangan<<" : Bilangan Genap"<<endl;
+	} else{
+		cout<<bilangan<<" : Bilangan Ganjil"<<endl;
+	}
+}
